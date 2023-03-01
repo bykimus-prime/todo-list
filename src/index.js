3180206f4bbb241e43a2d399b48001be088b9e76
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-03-01 16:46:18 +0900

    Add ability to create new projects

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { createEventListener } from "./projects";
+
+createEventListener();
---
+++ src/projects.js
@@ -0,0 +1,34 @@
+// creates event listeners for making a new project
+const createEventListener = () => {
+   const submitForm = document.getElementById('projectForm');
+   submitForm.addEventListener('submit', processNewProject);
+}
+
+const processNewProject = (e) => {
+   let newProjectInput = document.getElementById('projectInput').value;
+   addProject(newProjectInput);
+   e.preventDefault();
+}
+
+// create project and add to projects list in html
+const addProject = (textInput) => {
+   let datasetNum = incrementDataset();
+
+   const projectsList = document.querySelector('.projects-list');
+
+   const projectInfo = document.createElement('div');
+   projectInfo.classList.add('project-info');
+   projectInfo.setAttribute('data-project', `${datasetNum}`);
+   projectsList.appendChild(projectInfo);
+
+   const projectName = document.createTextNode(textInput);
+   projectInfo.appendChild(projectName);
+}
+
+//find next data-set
+const incrementDataset = () => {
+   const allProjects = document.querySelectorAll('[data-project]');
+   return allProjects.length;
+}
+
+export { createEventListener };
