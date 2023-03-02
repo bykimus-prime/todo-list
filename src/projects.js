b5ad59ec53fbf2b650238c2fac89533fa2471da0
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-03-02 15:56:23 +0900

    Separate dom manipulation from projects script

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,5 @@
+import { createProjectDiv } from "./DOMcontroller";
+
 let userAddedProjects = [];
 
 class Project {
@@ -24,29 +26,6 @@ const displayProjects = () => {
       createProjectDiv(project, index);
    });
    // removeBook();
-}
-
-const createProjectDiv = (project, index) => {
-   const userProjects = document.querySelector('.user-projects');
-   // create card div
-   const userProject = document.createElement('div');
-   userProject.classList.add('user-project');
-
-   // add book info to cards
-   const projectTitle = document.createElement('p');
-   projectTitle.classList.add('project-title');
-   projectTitle.innerText = `${project.projectTitle}`;
-   userProject.append(projectTitle);
-
-   // add remove button to card
-   const rmvProjectBtn = document.createElement('button');
-   rmvProjectBtn.setAttribute('data', index);
-   rmvProjectBtn.classList.add('remove-project-btn');
-   rmvProjectBtn.textContent = 'Delete Project';
-   userProject.appendChild(rmvProjectBtn);
-
-   // finally add book to book grid
-   userProjects.append(userProject);
 }
 
 export { addProject };
