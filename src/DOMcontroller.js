d682b00b78d6ed7d01c878b65b41439be0246983
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-03-02 15:57:56 +0900

    Clean code

--- src/DOMcontroller.js
+++ src/DOMcontroller.js
@@ -1,23 +1,23 @@
 const createProjectDiv = (project, index) => {
    const userProjects = document.querySelector('.user-projects');
-   // create card div
+   // create project div
    const userProject = document.createElement('div');
    userProject.classList.add('user-project');
 
-   // add book info to cards
+   // add project info to div
    const projectTitle = document.createElement('p');
    projectTitle.classList.add('project-title');
    projectTitle.innerText = `${project.projectTitle}`;
    userProject.append(projectTitle);
 
-   // add remove button to card
+   // add remove button to div
    const rmvProjectBtn = document.createElement('button');
    rmvProjectBtn.setAttribute('data', index);
    rmvProjectBtn.classList.add('remove-project-btn');
    rmvProjectBtn.textContent = 'Delete Project';
    userProject.appendChild(rmvProjectBtn);
 
-   // finally add book to book grid
+   // finally add project to projects list
    userProjects.append(userProject);
 }
 
